f5264c232a2f2662a75a1f477c6ed672906fd8db
GoogelTrad <celianmichez@free.fr>
2023-11-22 15:11:06 +0100

    last push?

--- src/main.c
+++ src/main.c
@@ -12,11 +12,31 @@
 
 #include "cub3d.h"
 
+void	fileok(char **av)
+{
+	if (access(av[1], F_OK) == -1)
+	{
+		printf("%s\n", "Error\n File does not exist");
+		exit(1);
+	}
+	if (access(av[1], R_OK) == -1)
+	{
+		printf("%s\n", "Error\n File cannot be read");
+		exit(1);
+	}
+	if (open(av[1], O_RDONLY) < 0)
+	{
+		printf("%s\n", "Error\n .cub file CHMOD problem");
+		exit(1);
+	}
+}
+
 int	main(int ac, char **av)
 {
 	t_data	data;
 
 	ft_parsing(ac, av, &data);
+	fileok(av);
 	if (ac > 2 || ac == 1)
 	{
 		printf("%s \n", "Error\nPlease only .cub map as argument");
